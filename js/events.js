04198dc423cb53ee560a4c0d682e5d0a7163df4c
David Reep <davereep@gmail.com>
2017-09-26 00:41:14 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,7 +13,7 @@ function frameIt() {
 
 function pressIt() {
   $('#typing').on("keydown", function(key) {
-    if(key.which == 103){
+    if(key.which == 71){
       alert('g was pressed');
       return;
     }
